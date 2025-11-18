8720ee14ef3fc48f4aa16e0121f9d3c7e905002c
c0a241379e <c0a241379e@edu.teu.ac.jp>
2025-11-18 16:49:03 +0900

    練習8:こうかとん移動

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -10,6 +10,7 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img_r = pg.transform.flip(bg_img, True, False)
     
@@ -17,30 +18,20 @@ def main():
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
 
-    bg_x = 0
-    speed = 5
-
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        #背景を左に流す
-        bg_x -= speed
-        if bg_x <= -1600:  # 背景画像の幅分ずれたらリセット
-            bg_x = 0
-        #背景を二枚準備
-        screen.blit(bg_img, [bg_x, 0])
-        #残像の補正
-        screen.blit(bg_img_r, [bg_x + 1600, 0])
-        #こうかとんを表示
-        screen.blit(kk_img, [300, 200]) 
+
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img_r, [-x + 1600, 0])
+        screen.blit(bg_img, [-x + 3200, 0])
+        screen.blit(kk_img, [300, 200])
 
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(200)
-        
-
-
 
 
 if __name__ == "__main__":
