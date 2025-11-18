24b500c0a2e3bc0768b23050888c7b18ce8f03ac
c0a241379e <c0a241379e@edu.teu.ac.jp>
2025-11-18 16:07:03 +0900

    練習4:こうかとんの描画

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -5,25 +5,37 @@ import pygame as pg
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-kk_img = pg.image.load("fig/3.png")
-kk_img = pg.transform.flip(kk_img,True,False)
-kk_img = pg.transform.rotozoom(kk_img,10,1.0)
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img,True,False)
+    kk_img = pg.transform.rotozoom(kk_img,10,1.0)
+
+    bg_x = 0
+    speed = 5
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        #背景を左に流す
+        bg_x -= speed
+        if bg_x <= -800:  # 背景画像の幅分ずれたらリセット
+            bg_x = 0
+        #背景を二枚準備
+        screen.blit(bg_img, [bg_x, 0])
+        screen.blit(bg_img, [bg_x + 800, 0])
+        #こうかとんを表示
         screen.blit(kk_img, [300, 200]) 
+
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
         
 
 
